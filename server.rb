40feb0cfb94dd58dff37ce718dd7d6eb0ecfbbb7
Nick Alberts <nwalberts5@gmail.com>
2017-12-14 16:54:54 -0500

    blank slate

--- server.rb
+++ server.rb
@@ -3,48 +3,9 @@ require 'pry'
 require 'csv'
 
 get '/' do
-  redirect '/articles'
+  "It would be great if I redirected to the article page!"
 end
 
 get '/articles' do
-  @articles = CSV.readlines("articles.csv")
-
-  erb :index
+  "Welcome to the Articles Page"
 end
-
-get "/articles/new" do
-  erb :new
-end
-
-post "/articles/new" do
-  @title = params["article_title"]
-  @url = params["article_url"]
-  @description = params["article_description"]
-
-  if @title == "" || @url == "" || @description == ""
-    @error = "Please fill out all of the forms correctly!"
-    erb :new
-  else
-
-    CSV.open("articles.csv", "a") do |csv|
-      csv << [title, url, description]
-    end
-
-    redirect "/articles"
-  end
-
-end
-
-
-
-
-
-
-
-
-
-
-
-# CSV.foreach('articles.csv', headers: true, header_converters: :symbol) do |row|
-#   @articles << row.to_hash
-# end
--- topics.md
+++
@@ -1,8 +0,0 @@
-redirect from root path
-digging into the params hash
-how to read from a CSV
-validating for existence
-why re-render vs. redirect
-
-OPTIONAL
-re-rendering with variables on the page
